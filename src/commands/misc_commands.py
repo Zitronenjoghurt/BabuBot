a69eedfcc85ed4472807316369bbe1d799c2abb8
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-02-01 21:55:20 +0100

    add say command

--- src/commands/misc_commands.py
+++ src/commands/misc_commands.py
@@ -1,3 +1,5 @@
+import discord
+from discord import app_commands
 from discord.ext import commands
 
 class MiscCommands(commands.Cog):
@@ -7,7 +9,12 @@ class MiscCommands(commands.Cog):
     @commands.hybrid_command()
     async def ping(self, ctx):
         latency_ms = round(self.bot.latency * 1000)
-        await ctx.send(f"**Pong!** {latency_ms}ms\nI'll just be staying online this night as a test!")
+        await ctx.send(f"**Pong!** {latency_ms}ms\n")
+
+    @app_commands.command(name="say", description="Will just repeat whatever you want to say")
+    @app_commands.describe(message="Whatever message you want the bot to say")
+    async def say(self, interaction: discord.Interaction, message: str):
+        await interaction.response.send_message(f"{message}")
 
 async def setup(bot):
     await bot.add_cog(MiscCommands(bot))
