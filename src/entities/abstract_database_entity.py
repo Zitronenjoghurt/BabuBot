84129d09a82aa2cb8dc7214a6b1f3c45c4431df4
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-02-06 20:03:57 +0100

    propagate order and paging functionality to abstract database entity

--- src/entities/abstract_database_entity.py
+++ src/entities/abstract_database_entity.py
@@ -43,8 +43,8 @@ class AbstractDatabaseEntity(AbstractSerializableEntity):
         return cls.from_dict(data=data)
 
     @classmethod
-    def findall(cls, sort_key: Optional[str] = None, descending: bool = True, **kwargs) -> Any:
-        results = DB.findall(table_name=cls.TABLE_NAME, sort_key=sort_key, descending=descending, **kwargs)
+    def findall(cls, sort_key: Optional[str] = None, descending: bool = True, limit: Optional[int] = None, page: int = 1, **kwargs) -> Any:
+        results = DB.findall(table_name=cls.TABLE_NAME, sort_key=sort_key, descending=descending, limit=limit, page=page, **kwargs)
         if not results:
             return None
         
