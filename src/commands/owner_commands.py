502210355a3ba8f5cf8cbeab9fd2da3b0e40a42d
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-02-28 19:13:43 +0100

    enable bot to notify users in a safe way

--- src/commands/owner_commands.py
+++ src/commands/owner_commands.py
@@ -1,10 +1,23 @@
+import discord
 from discord.ext import commands
 from src.entities.user import User
 from src.logging.logger import LOGGER
+from src.utils.bot_operations import notify_user_private
 
 class OwnerCommands(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def notify(self, ctx, member: discord.Member, text: str):
+        if not isinstance(member, discord.Member):
+            return
+        success = await notify_user_private(bot=self.bot, user_id=member.id, message=text)
+        if success:
+            await ctx.send("Notified user.")
+        else:
+            await ctx.send("Unable to notify user.")
 
     @commands.command()
     @commands.is_owner()
--- src/utils/bot_operations.py
+++ src/utils/bot_operations.py
@@ -23,3 +23,74 @@ async def retrieve_guild_strict(bot: commands.Bot, guild_id: int) -> discord.Gui
         raise RuntimeError("Unable to retrieve guild.")
     
     return guild
+
+async def notify_user(bot: commands.Bot, user_id: int, try_dm: bool = False, channel_id: Optional[int] = None, message: Optional[str] = None, embed: Optional[discord.Embed] = None):
+    dm_success = None
+    if try_dm:
+        dm_success = await notify_user_private(bot=bot, user_id=user_id, message=message, embed=embed)
+        if dm_success:
+            return
+    
+    if not channel_id:
+        raise RuntimeError(f"An error occured while notifying user {user_id}: no channel id specified. try_dm: {try_dm}, dm_success: {dm_success}")
+    
+    channel_success = await notify_user_channel(bot=bot, user_id=user_id, channel_id=channel_id, message=message, embed=embed)
+    if not channel_success:
+        raise RuntimeError(f"Unable to notify user {user_id} in channel {channel_id}. try_dm: {try_dm}, dm_success: {dm_success}")
+
+async def notify_user_private(bot: commands.Bot, user_id: int, message: Optional[str] = None, embed: Optional[discord.Embed] = None) -> bool:
+    try:
+        user = await bot.fetch_user(user_id)
+        if not isinstance(user, discord.User):
+            LOGGER.error(f"Tried to notify user {user_id} via DM: unable to retrieve user.")
+            return False
+        dm_channel = await user.create_dm()
+
+        content_available = isinstance(message, str)
+        embed_available = isinstance(embed, discord.Embed)
+        if content_available and embed_available:
+            await dm_channel.send(content=message, embed=embed)
+        elif content_available:
+            await dm_channel.send(content=message)
+        elif embed_available:
+            await dm_channel.send(embed=embed)
+        else:
+            LOGGER.error(f"Tried to notify user {user_id} via DM: no message or embed was provided.")
+            return False
+        
+        return True
+    except Exception as e:
+        LOGGER.critical(f"An error occured while trying to notify user {user_id} via DM: {e}")
+        return False
+    
+async def notify_user_channel(bot: commands.Bot, user_id: int, channel_id: int, message: Optional[str] = None, embed: Optional[discord.Embed] = None) -> bool:
+    try:
+        user = await bot.fetch_user(user_id)
+        channel = await bot.fetch_channel(channel_id)
+
+        if not isinstance(user, discord.User):
+            LOGGER.error(f"Tried to notify user {user_id} in channel {channel_id}: unable to retrieve user.")
+            return False
+        if not isinstance(channel, discord.TextChannel):
+            LOGGER.error(f"Tried to notify user {user_id} in channel {channel_id}: unable to retrieve channel.")
+            return False
+        
+        content_available = isinstance(message, str)
+        embed_available = isinstance(embed, discord.Embed)
+        if not content_available and not embed_available:
+            LOGGER.error(f"Tried to notify user {user_id} in channel {channel_id}: unable to retrieve channel.")
+            return False
+
+        if content_available:
+            message = f"<@{user_id}>,\n{message}"
+        else:
+            message = f"<@{user_id}>"
+        
+        if embed_available:
+            await channel.send(content=message, embed=embed)
+        else:
+            await channel.send(content=message)
+        return True
+    except Exception as e:
+        LOGGER.critical(f"An error occured while trying to notify user {user_id} in channel {channel_id}: {e}")
+        return False
