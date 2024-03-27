a0fab94181ac6217f45ae5ec102e64b9553782c8
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-03-27 20:18:02 +0100

    add command to match a pokemon name

--- src/commands/pokemon_commands.py
+++ src/commands/pokemon_commands.py
@@ -1,17 +1,19 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from typing import Optional
 from src.constants.custom_embeds import ErrorEmbed
+from src.constants.pokemon_names import PokemonNames
 from src.entities.pokemon.pokemon import Pokemon
 from src.ui.pokedex_view import send_pokedex_view
+
+PN = PokemonNames.get_instance()
 
 class PokemonCommands(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
     
     @app_commands.command(name="pokedex", description="Get all important information about a pokemon.")
-    @app_commands.describe(name="Name of the pokemon in en, de or fr. Can include typos.")
+    @app_commands.describe(name="Name of the pokemon. Can include typos.")
     @app_commands.checks.cooldown(1, 5)
     async def pokdedex(self, interaction: discord.Interaction, name: str):
         await interaction.response.defer()
@@ -21,6 +23,38 @@ class PokemonCommands(commands.Cog):
             return await interaction.followup.send(embed=ErrorEmbed(title="Pokemon not found", message="The specified pokemon does not exist. The typo-correction only recognizes en, de and fr pokemon names."))
         
         await send_pokedex_view(interaction=interaction, pokemon=pokemon)
+
+    @app_commands.command(name="pokemon-name", description="Find the closest pokemon name to your provided name.")
+    @app_commands.describe(name="Name of the pokemon. Can include typos.")
+    @app_commands.checks.cooldown(1, 5)
+    async def pokemon_name(self, interaction: discord.Interaction, name: str):
+        await interaction.response.defer()
+
+        result = PN.match_name_extended(name=name)
+        if result is None:
+            return await interaction.followup.send(embed=ErrorEmbed(title="Pokemon not found", message="The provided name does not match any pokemon name in the slightest."))
+        
+        final_name, matched_name, score = result
+
+        match = matched_name.capitalize()
+        final = format_name(final_name)
+
+        description = ""
+        if match != final:
+            description = f"*matched from* **`{match}`**\n"
+        description += f"Confidence: **`{score}%`**"
+
+        embed = discord.Embed(
+            title=final,
+            description=description,
+            color=discord.Color.from_str("#74F2C8")
+        )
+        embed.set_author(name="POKEMON NAME GUESS")
+        await interaction.followup.send(embed=embed)
+
+def format_name(name: str) -> str:
+    words = name.replace('-', ' ').split()
+    return ' '.join(word.capitalize() for word in words)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(PokemonCommands(bot))
--- src/constants/pokemon_names.py
+++ src/constants/pokemon_names.py
@@ -1,4 +1,4 @@
-from difflib import get_close_matches
+from fuzzywuzzy import process
 from typing import Optional
 from src.utils.file_operations import construct_path, file_to_dict
 
@@ -21,7 +21,19 @@ class PokemonNames():
     
     def match_name(self, name: str) -> Optional[str]:
         name = name.lower()
-        matched_names = get_close_matches(word=name, possibilities=self.names, n=1)
-        if not matched_names:
+        result = process.extractOne(name, self.names, score_cutoff=40)
+        if not result:
             return
-        return self.names_map.get(matched_names[0], None)
+        matched_name, score = result # type: ignore
+        return self.names_map.get(matched_name, None)
+    
+    def match_name_extended(self, name: str) -> Optional[tuple[str, str, float]]:
+        name = name.lower()
+        result = process.extractOne(name, self.names, score_cutoff=10)
+        if not result:
+            return
+        matched_name, score = result # type: ignore
+        final_name = self.names_map.get(matched_name, None)
+        if final_name is None:
+            return None
+        return (final_name, matched_name, score)
